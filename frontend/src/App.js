272d9d9aaec0267b9ada833f069e6f9616dd53a1
reqtear <dannywijaya.9h.6@gmail.com>
2021-04-16 21:19:32 +0700

    Frontend_pending_v1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Route} from 'react-router-dom'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 function App() {
   return (
-    <>
-      <h1>Welcome to ProShop</h1>
-    </>
+    <Router>
+      <Header />
+      <Container>
+        <main className="py-3">
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
+        </main>
+      </Container>
+      <Footer />
+    </Router>
   );
 }
 
