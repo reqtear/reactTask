f7a8a2934054298f05e07f05080cf685a42d01d7
reqtear <dannywijaya.9h.6@gmail.com>
2021-04-16 22:21:54 +0700

    FrontEnd_pending_v2

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -12,13 +12,14 @@ const Rating = ({ value, text, color }) => {
                 <i style={{color}} className={value >= 5 ? 'fas fa-star' : value >= 4.5 ? 'fas fa-star-half-alt' : 'far fa-star'}></i>
             </span>
             <span>{ text && text }</span>
+            {/* Jika ada text maka ditampilkan, jika tidak ada maka tidak ditampilkan */}
         </div>
     )
 }
 
-Rating.defaultProps = {
-    color: '#f8e825',
-}
+// Rating.defaultProps = {
+//     color: '#f8e825',
+// }
 
 Rating.propTypes = {
     value: PropTypes.number.isRequired,
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,54 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Col, Row, Button, Image, ListGroup, Card } from 'react-bootstrap'
+import Rating from '../components/Rating'
+import products from '../products'
 
-const ProductScreen = () => {
+const ProductScreen = ({ match }) => {
+    const product = products.find(p => p._id === match.params.id)
     return (
-        <div>
-            Product
-        </div>
+        <>
+        <Row className='my-3'>
+            <Col md={6}>
+                <Image src={product.image} alt={product.name} fluid />
+            </Col>
+            <Col md={3}>
+                <ListGroup variant='flush'>
+                    <ListGroup.Item>
+                        <h3>{product.name}</h3>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        ${product.price}
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        {product.description}
+                    </ListGroup.Item>
+                </ListGroup>
+            </Col>
+            <Col md={3}>
+                <Card className='rounded'>
+                    <ListGroup variant='flush'>
+                        <ListGroup.Item>
+                            Price: ${product.price}
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            Status: {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <button className='btn btn-dark btn-block' type='button'
+                            disabled={product.countInStock === 0}>Add To Cart</button>
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Card>
+            </Col>
+        </Row>
+        <Link className='btn btn-dark my-3' to='/'>Go Back</Link>
+        </>
     )
 }
 
 export default ProductScreen
+ 
